a1194cad78c695a555949c30a8f47480c7bf9851
myxklinten <my@yxklinten.com>
2019-04-07 12:36:27 +0100

    Updated JS file with list + random function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,12 @@
-console.log("hej")
+var titleList = ["Eucalyptus", "Aloe Vera"]
+var subHeaderList = ["Eucalyptus globulus", "Aloe perfoliata L"]
+var imageList = ["../images/eucalyptus.jpg", "../images/aloe.jpg"]
+var textList = ["Eucalyptus is a genus of over seven hundred species of flowering trees, shrubs or mallees in the myrtle family, Myrtaceae commonly known as eucalypts.", "Aloe vera is a succulent plant species of the genus Aloe. An evergreen perennial, it originates from the Arabian Peninsula but grows wild in tropical climate."]
 
 function displayResult() {
-  document.getElementById("myHeader").innerHTML = "Aloe Vera";
-  document.getElementById("subHeader").innerHTML = "Aloe perfoliata L";
-  document.getElementById("infoImage").src = "../images/aloe.jpg";
-  document.getElementById("infoText").innerHTML = "Aloe vera is a succulent plant species of the genus Aloe. An evergreen perennial, it originates from the Arabian Peninsula but grows wild in tropical climate.";
+  var index = Math.floor(Math.random() * 2)
+  document.getElementById("myHeader").innerHTML = titleList[index];
+  document.getElementById("subHeader").innerHTML = subHeaderList[index];
+  document.getElementById("infoImage").src = imageList[index];
+  document.getElementById("infoText").innerHTML = textList[index];
     }
