bb36b5a5d3fef39b433f6c7abafcef856faf0b9c
myxklinten <my@yxklinten.com>
2019-04-07 12:05:37 +0100

    Updated JS file

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+console.log("hej")
+
+function displayResult() {
+  document.getElementById("myHeader").innerHTML = "Aloe Vera";
+  document.getElementById("subHeader").innerHTML = "Aloe perfoliata L";
+  document.getElementById("infoImage").src = "../images/aloe.jpg";
+  document.getElementById("infoText").innerHTML = "Aloe vera is a succulent plant species of the genus Aloe. An evergreen perennial, it originates from the Arabian Peninsula but grows wild in tropical climate.";
+    }
